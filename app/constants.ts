9deb97024d34a5a24edadf164ddd1cf0cb1a09b6
JcaveroR <jcaveror@gmail.com>
2021-10-17 13:11:36 -0500

    Update constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -2,7 +2,7 @@ export const INFORMATION = {
   avatar: "/assets/avatar.jpg",
   banner: "/assets/banner.jpg",
   title: "Cielito Azul",
-  description: "Galardonada pizza estilo New Haven en Palermo SOHO.",
+  description: "Calidad y Garantía en todos nuestros productos.",
   phone: `51928717437`,
   sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vTnNtHzoeWLIUAPm1kQjQUYVfA0XRcV85Z07fZg4tDTf_M4KXbcdm1WcJSx8DeiCvHAeHebB1xsv7qQ/pubhtml`,
   color: `teal`,
