6272c5d980ccbe90fc62704240fd8438bf54a4ca
JcaveroR <jcaveror@gmail.com>
2021-10-17 19:50:28 -0500

    Update constants.ts
    datos de sanguchito

--- app/constants.ts
+++ app/constants.ts
@@ -1,19 +1,19 @@
 export const INFORMATION = {
   avatar: "/assets/avatar.jpg",
   banner: "/assets/banner.jpg",
-  title: "Cielito Azul",
+  title: "SanGuchito",
   description: "Calidad y Garantía en todos nuestros productos.",
-  phone: `51928717437`,
-  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vTnNtHzoeWLIUAPm1kQjQUYVfA0XRcV85Z07fZg4tDTf_M4KXbcdm1WcJSx8DeiCvHAeHebB1xsv7qQ/pub?output=csv`,
+  phone: `51949334433`,
+  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQAIpI_tPm8EqVEUE2XgJp0PiC3uEl-KqOhZ_LMXIvt1yn56M-YXGG92vlbL62KaALX2MmmA4r1JMeZ/pub?output=csv`,
   color: `teal`,
   social: [
     {
-      name: "instagram",
-      url: `https://instagram.com/cielitoazul.js`,
+      name: "facebook",
+      url: `https://www.facebook.com/sanguchitobarranca/`,
     },
     {
       name: "whatsapp",
-      url: `https://wa.me/51928717437`,
+      url: `https://wa.me/51949334433`,
     },
   ],
 };
